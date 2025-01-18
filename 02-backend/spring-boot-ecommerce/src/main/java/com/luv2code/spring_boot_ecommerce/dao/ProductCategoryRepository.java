3b5814d470d87968dfa7c0abb77297cfff2eb2fa
tan nguyen <bivani2805@gmail.com>
2025-01-18 07:29:17 +0000

    Add cross origin for back end and front end connection

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/spring_boot_ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/spring_boot_ecommerce/dao/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.luv2code.spring_boot_ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.luv2code.spring_boot_ecommerce.entity.ProductCategory;
 
+@CrossOrigin("https://4200-grecotan12-fullstackeco-lkahhatu5a3.ws-us117.gitpod.io/")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/spring_boot_ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/spring_boot_ecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.luv2code.spring_boot_ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.luv2code.spring_boot_ecommerce.entity.Product;
 
+@CrossOrigin("https://4200-grecotan12-fullstackeco-lkahhatu5a3.ws-us117.gitpod.io/")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
